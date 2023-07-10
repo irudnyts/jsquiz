ade85389cb6274c30cb9d1275b379cb2f24bb69b
Iegor Rudnytskyi <yegor.rudnytskiy@gmail.com>
2023-07-10 16:52:27 +0200

    Use pipe instead of intermediate vars and add additional check whether `javascript` is already included in the header

--- R/add_js.R
+++ R/add_js.R
@@ -1,29 +1,32 @@
 add_js <- function() {
 
     # copy javascript.html
-    javascript_html <- system.file("javascript.html", package = "jsquiz")
-    file.copy(javascript_html, getwd())
+    system.file("javascript.html", package = "jsquiz") %>%
+        file.copy(getwd())
 
     # copy quiz.js
-    quiz_js <- system.file("quiz.js", package = "jsquiz")
-    file.copy(quiz_js, getwd())
+    system.file("quiz.js", package = "jsquiz") %>%
+        file.copy(getwd())
 
     # copy checkOrder.js
-    checkOrder <- system.file("checkOrder.js", package = "jsquiz")
-    file.copy(checkOrder, getwd())
+    system.file("checkOrder.js", package = "jsquiz") %>%
+        file.copy(getwd())
 
     # copy dragndrop.js
-    dragndrop <- system.file("dragndrop.js", package = "jsquiz")
-    file.copy(dragndrop, getwd())
+    system.file("dragndrop.js", package = "jsquiz") %>%
+        file.copy(getwd())
 
     # modify _output.yml
     output_yaml <- "_output.yml"
     config <- yaml::read_yaml(output_yaml)
 
-    config$`bookdown::gitbook`$includes$in_header <- append(
-        config$`bookdown::gitbook`$includes$in_header,
-        "javascript.html"
-    )
+    if (!("javascript.html" %in%
+          config$`bookdown::gitbook`$includes$in_header)) {
+        config$`bookdown::gitbook`$includes$in_header <- append(
+            config$`bookdown::gitbook`$includes$in_header,
+            "javascript.html"
+        )
+    }
 
     yaml::write_yaml(config, output_yaml)
 }
